86bf78e2031f5a8975c21a2192697e7f22200423
Ferdinand <ndimbecha@gmail.com>
2021-09-13 11:39:17 +0200

    Another commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-import http from 'http'
+import dotenv from 'dotenv'
+ import http from 'http'
+
+ // Deploy code
+dotenv.config()
+console.log("API_KEY is " + process.env.API_KEY)
+
+// Deploy code end
+
+
 
 function requestDealer(req, resp) {
 	const randomNumber= (Math.ceil(Math.random()*1000000000))
 	resp.end(randomNumber.toString())
 	
 }
+const port =8081
 const server =http.createServer(requestDealer)
-server.listen(8081)
+console.log("Server listening on localhost:"+port)
+server.listen(port) 
